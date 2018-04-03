7c9ef58fcc5d5aba8a5447f2c3f5821a1839022f
Rashed Abdel-Tawab <rashed@linux.com>
2018-04-03 11:41:07 -0700

    h870: Override all fingerprints
    Change-Id: Ia3a7cc7a80991412229d7cb4e6f6dd32ee5fefd3

--- lineage.mk
+++ lineage.mk
@@ -11,5 +11,6 @@ PRODUCT_NAME := lineage_h870
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g6" \
     PRODUCT_NAME="lucye_global_com" \
-    BUILD_FINGERPRINT="lge/lucye_global_com/lucye:7.0/NRD90U/171732341982c:user/release-keys" \
     PRIVATE_BUILD_DESC="lucye_global_com-user 7.0 NRD90U 171732341982c release-keys"
+
+BUILD_FINGERPRINT := "lge/lucye_global_com/lucye:7.0/NRD90U/171732341982c:user/release-keys"
