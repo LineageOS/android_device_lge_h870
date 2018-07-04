c0fe138bc374220d5e1ceda1ea4f2399ced3c6f3
Rashed Abdel-Tawab <rashed@linux.com>
2018-07-04 09:20:53 -0700

    h870: Move sensor config to vendor
    Change-Id: I652d3489e8ed10fc1a6ee0a2d55cae28f1f1607a

--- device.mk
+++ device.mk
@@ -27,7 +27,7 @@ $(call inherit-product, device/lge/g6-common/g6.mk)
 
 # Sensors
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sensors/sensor_def_lucye_global_com.conf:system/etc/sensors/sensor_def_variable.conf
+    $(LOCAL_PATH)/configs/sensors/sensor_def_lucye_global_com.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_variable.conf
 
 # WiFi Calibration
 PRODUCT_COPY_FILES += \
--- vendor_prop.mk
+++ vendor_prop.mk
@@ -4,5 +4,4 @@
 
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.telephony.default_network=22 \
     telephony.lteOnGsmDevice=1
